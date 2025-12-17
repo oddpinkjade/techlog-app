fc89c30bb5f7aed1080a99adc69d17302291d824
cutehumanS2 <oddpinksyrup@gmail.com>
2025-12-17 09:48:16 +0900

    ログイン画面の修正
    しかし、正常系のテストが失敗している

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -73,7 +73,7 @@ describe 'User', type: :system do
         let(:password) { 'a' * 129 }
         it 'ユーザーを作成せず、エラーメッセージを表示する' do
           expect { subject }.not_to change(User, :count)
-          expect(page).to have_content('Password is too long (maximum is 128 characters)')
+          expect(page).to have_content('Password is too long (maximum is 128 characters')
         end
       end
 
@@ -83,6 +83,32 @@ describe 'User', type: :system do
           expect { subject }.not_to change(User, :count)
           expect(page).to have_content("Password confirmation doesn't match Password")
         end
+      end
+    end
+  end
+
+  describe 'ログイン機能の検証' do
+    #let(:email) { 'login_test@example.com' }
+    # 事前にユーザー作成
+    before do
+      create(:user, nickname: nickname, email: email, password: password, password_confirmation: password) # 事前にユーザー作成
+
+      visit '/users/sign_in'
+      fill_in 'user_email', with: email
+      fill_in 'user_password', with: 'password'
+      click_button 'ログイン'
+    end
+
+    context '正常系' do
+      it 'ログインに成功し、トップページにリダイレクトする' do
+        expect(current_path).to eq('/')
+      end
+    end
+
+    context '異常系' do
+      let(:password) { 'NGpassword' }
+      it 'ログインに失敗し、ページ遷移しない' do
+        expect(current_path).to eq('/users/sign_in')
       end
     end
   end
